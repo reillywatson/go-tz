b9be4dd1d02157cfc3ee3d7ec71b1e48bd1bb0e4
ugjka <esesmu@protonmail.com>
2018-11-21 18:16:18 +0200

    reduce allocations

--- geojson.go
+++ geojson.go
@@ -35,30 +35,32 @@ type geometry struct {
 	Coordinates [][]Point `json:"coordinates,omitempty"`
 }
 
+var jPolyType struct {
+	Type       string      `json:"type"`
+	Geometries []*Geometry `json:"geometries,omitempty"`
+}
+
+var jPolygon struct {
+	Coordinates [][][]float64 `json:"coordinates,omitempty"`
+}
+
+var jMultiPolygon struct {
+	Coordinates [][][][]float64 `json:"coordinates,omitempty"`
+}
+
 func (g *Geometry) UnmarshalJSON(data []byte) (err error) {
-	var polyType struct {
-		Type       string      `json:"type"`
-		Geometries []*Geometry `json:"geometries,omitempty"`
-	}
-	if err := json.Unmarshal(data, &polyType); err != nil {
+	if err := json.Unmarshal(data, &jPolyType); err != nil {
 		return err
 	}
 	g.Type = "MultiPolygon"
-	var polygon struct {
-		Coordinates [][][]float64 `json:"coordinates,omitempty"`
-	}
 
-	var multiPolygon struct {
-		Coordinates [][][][]float64 `json:"coordinates,omitempty"`
-	}
-
-	if polyType.Type == "Polygon" {
-		if err := json.Unmarshal(data, &polygon); err != nil {
+	if jPolyType.Type == "Polygon" {
+		if err := json.Unmarshal(data, &jPolygon); err != nil {
 			return err
 		}
 		//Create a bounding box
-		pol := make([]Point, 0)
-		for _, v := range polygon.Coordinates[0] {
+		pol := make([]Point, 0, 50)
+		for _, v := range jPolygon.Coordinates[0] {
 			pol = append(pol, Point{v[0], v[1]})
 		}
 		b := getBoundingBox(pol)
@@ -67,12 +69,12 @@ func (g *Geometry) UnmarshalJSON(data []byte) (err error) {
 		return nil
 	}
 
-	if polyType.Type == "MultiPolygon" {
-		if err := json.Unmarshal(data, &multiPolygon); err != nil {
+	if jPolyType.Type == "MultiPolygon" {
+		if err := json.Unmarshal(data, &jMultiPolygon); err != nil {
 			return err
 		}
-		for _, poly := range multiPolygon.Coordinates {
-			pol := make([]Point, 0)
+		for _, poly := range jMultiPolygon.Coordinates {
+			pol := make([]Point, 0, 50)
 			for _, v := range poly[0] {
 				pol = append(pol, Point{v[0], v[1]})
 			}
